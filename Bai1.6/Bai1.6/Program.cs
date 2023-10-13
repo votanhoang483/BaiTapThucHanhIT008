be2020bd11a64234d2bd32daacafbfa3809ed647
votanhoang483 <22520483@gm.uit.edu.vn>
2023-10-13 16:00:26 +0700

    Tan Hoang

---
+++ Bai1.6/Bai1.6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1._6
+{
+    internal class Program
+    {
+        interface IThinking
+        {
+            void thinking_behavior();
+        }
+        interface IIntellingent
+        {
+            void intelligent_behavior();
+        }
+        interface IAbility: IThinking, IIntellingent
+        {
+
+        }
+        public class Mamal
+        {
+            public string characteristics;
+        }
+        public class Whale: Mamal
+        {
+            public Whale()
+            {
+
+            }
+        }
+        public class Human: Mamal, IAbility
+        {
+            public Human()
+            {
+
+            }
+            public void intelligent_behavior()
+            {
+
+            }
+            public void thinking_behavior()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
