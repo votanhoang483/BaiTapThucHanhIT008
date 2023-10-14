e6bb2b35202952160af557a87743e56e6cbb68e7
PhanTranTienHung22520523 <22520523@gm.uit.edu.vn>
2023-10-15 00:31:22 +0700

    Bai1.1 va Bai1.2

---
+++ Lab1/Lab1_LTTQ_Bai 1.1/Bai 1.1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+
+namespace hinhhoc
+{
+    public class Diem
+    {
+        public float a, b;
+        public void nhapdiem()
+        {
+            Console.Write("Nhap toa do cua x: ");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Nhap toa do cua y: ");
+            b = Convert.ToSingle(Console.ReadLine());
+        }
+        public void xuatdiem()
+        {
+            Console.WriteLine("({0),{1}", a, b);
+        }
+    }
+    public abstract class Shape
+    {
+        public float dai, rong, bankinh;
+        public string ten;
+        public virtual void nhap()
+        {
+
+        }
+        public virtual void tinhS()
+        {
+            
+        }
+        public virtual void ve()
+        {
+
+        }
+    }
+    public class hinhchunhat : Shape 
+    {
+        public Diem d = new Diem();
+        public override void nhap()
+        {
+            ten = "Hinh Chu Nhat";
+            d.nhapdiem();
+            Console.Write("Nhap chieu dai: ");
+            dai= Convert.ToSingle(Console.ReadLine());
+            Console.Write("Nhap chieu rong: ");
+            rong=Convert.ToSingle(Console.ReadLine());
+        }
+        public override void ve()
+        {
+            Console.WriteLine(ten);
+            Console.WriteLine("Toa do diem thu nhat la ({0},{1}) ", d.a+rong, d.b+dai);
+            Console.WriteLine("Toa do diem thu hai la ({0},{1}) ", d.a+rong, d.b);
+            Console.WriteLine("Toa do diem thu ba la ({0},{1}) ", d.a, d.b+dai);
+            Console.WriteLine("Toa do diem thu tu la ({0},{1}) ", d.a, d.b);
+        }
+        public override void tinhS()
+        {
+            Console.WriteLine("Dien tich cua hinh chu nhat la: {0}", dai * rong);
+        }
+    }
+    public class hinhvuong: hinhchunhat
+    {
+        public override void nhap()
+        {
+            ten = "Hinh Vuong";
+            d.nhapdiem();
+            Console.Write("Nhap chieu dai: ");
+            dai = Convert.ToSingle(Console.ReadLine());
+        }
+        public override void ve()
+        {
+            Console.WriteLine(ten);
+            Console.WriteLine("Toa do diem thu nhat la ({0},{1}) ", d.a + dai, d.b + dai);
+            Console.WriteLine("Toa do diem thu hai la ({0},{1}) ", d.a + dai, d.b);
+            Console.WriteLine("Toa do diem thu ba la ({0},{1}) ", d.a, d.b + dai);
+            Console.WriteLine("Toa do diem thu tu la ({0},{1}) ", d.a, d.b);
+        }
+        public override void tinhS()
+        {
+            Console.WriteLine("Dien tich cua hinh vuong la: {0}", dai * dai);
+        }
+    }
+    public class hinhtamgiac : Shape
+    {
+        readonly public Diem[] d = new Diem[3];
+        public override void nhap()
+        {
+            ten = "Hinh Tam Giac";
+            for(int i=0;i<3;i++)
+            {
+                d[i] = new Diem();
+                Console.WriteLine("Nhap toa do diem thu {0}", i+1);
+                d[i].nhapdiem();
+            }
+        }
+        public override void ve()
+        {
+            Console.WriteLine(ten);
+            Console.WriteLine("Toa do diem thu nhat la ({0},{1}) ", d[0].a , d[0].b );
+            Console.WriteLine("Toa do diem thu hai la ({0},{1}) ", d[1].a , d[1].b);
+            Console.WriteLine("Toa do diem thu ba la ({0},{1}) ", d[2].a, d[2].b );
+        }
+        public override void tinhS()
+        {
+            float canh1 = (float)Math.Sqrt((d[0].a - d[1].a) * (d[0].a - d[1].a) + (d[0].b - d[1].b) * (d[0].b - d[1].b));
+            float canh2 = (float)Math.Sqrt((d[0].a - d[2].a) * (d[0].a - d[2].a) + (d[0].b - d[2].b) * (d[0].b - d[2].b));
+            float canh3 = (float)Math.Sqrt((d[2].a - d[1].a) * (d[2].a - d[1].a) + (d[2].b - d[1].b) * (d[2].b - d[1].b));
+            float s = (canh1 + canh2 + canh3) / 3;
+            float kq = (float)Math.Sqrt(s * Math.Abs(s - canh1) * Math.Abs(s - canh2) * Math.Abs(s - canh3));
+            Console.WriteLine("Dien tich cua hinh tam giac la: {0}", kq);
+        }
+    }
+    public class hinhtron : Shape
+    {
+        Diem d=new Diem();
+        public override void nhap()
+        {
+            ten = "Hinh Tron";
+            Console.WriteLine("Nhap toa do tam duong tron");
+            d.nhapdiem();
+            Console.Write("Nhap ban kinh: ");
+            bankinh = Convert.ToSingle(Console.ReadLine());
+        }
+        public override void ve()
+        {
+            Console.WriteLine(ten);
+            Console.WriteLine("Toa do tam duong tron la ({0},{1}) ", d.a, d.b);;
+        }
+        public override void tinhS()
+        {
+            float kq = bankinh * bankinh * (float)3.14;
+            Console.WriteLine("Dien tich cua hinh tron la: {0}", kq);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so hinh can tao: ");
+            int so=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Shape[] sh = new Shape[so];
+            for(int i=0;i<so;i++)
+            {
+                Console.WriteLine("Nhap thong tin hinh thu {0}", i + 1);
+                Random r=new Random();
+                int ra = r.Next(1, 4);
+                if(ra == 1)
+                {
+                    sh[i] = new hinhchunhat();
+                }
+                if (ra == 2)
+                {
+                    sh[i] = new hinhvuong();
+                }
+                if(ra==3)
+                {
+                    sh[i]=new hinhtamgiac();
+                }
+                if(ra==4)
+                {
+                    sh[i] = new hinhtron();
+                }
+                sh[i].nhap();
+                Console.WriteLine();
+            }
+            for(int i=0;i<so;i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Hinh thu {0}\n", i+1);
+                sh[i].ve();
+                sh[i].tinhS();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/Lab1_LTTQ_Bai 1.2/Bai 1.2.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace tinhphanso
+{
+    class phanso
+    {
+        public int tu {  get; set; }
+        public int mau {  get; set; }
+        public void nhap()
+        {
+            Console.Write("Nhap tu so: ");
+            tu=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap mau so: ");
+            do
+            {
+                mau = Convert.ToInt32(Console.ReadLine());
+                if(mau==0)
+                {
+                    {
+                        Console.Write("Mau so bat buoc phai khac 0, vui long nhap lai mau so: ");
+                    }
+                }
+            }
+            while (mau == 0);
+            
+
+        }
+        public void xuat()
+        {
+            Console.WriteLine("Phan So: " + tu + "/" + mau);
+        }
+        public static int GCD(int x, int y)
+        {
+            int a = Math.Abs(x);
+            int b = Math.Abs(y);
+            if (a == 0 || b == 0)
+                return a + b;
+            else
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a = a - b;
+                    else
+                        b = b - a;
+                }
+                return a;
+            }
+        }
+        public static phanso operator +(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp.tu = p1.tu * p2.mau + p1.mau * p2.tu;
+            tmp.mau = p1.mau * p2.mau;
+            int gcd = GCD(tmp.tu, tmp.mau) ;
+            tmp.tu = tmp.tu / gcd;
+            tmp.mau = tmp.mau / gcd;
+            return tmp;
+        }
+        public static phanso operator -(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp.tu = p1.tu * p2.mau - p1.mau * p2.tu;
+            tmp.mau = p1.mau * p2.mau;
+            int gcd = GCD(tmp.tu, tmp.mau);
+            tmp.tu = tmp.tu / gcd;
+            tmp.mau = tmp.mau / gcd;
+            return tmp;
+        }
+        public static phanso operator *(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp.tu = p1.tu * p2.tu;
+            tmp.mau = p1.mau * p2.mau;
+            int gcd = GCD(tmp.tu, tmp.mau);
+            tmp.tu = tmp.tu / gcd;
+            tmp.mau = tmp.mau / gcd;
+            return tmp;
+        }
+        public static phanso operator /(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp.tu = p1.tu * p2.mau;
+            tmp.mau = p1.mau * p2.tu;
+            int gcd = GCD(tmp.tu, tmp.mau);
+            tmp.tu = tmp.tu / gcd;
+            tmp.mau = tmp.mau / gcd;
+            return tmp;
+        }
+        public static bool operator ==(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp = p1 - p2;
+            if (tmp.tu == 0)
+                return true;
+            else
+                return false;
+
+        }
+        public static bool operator !=(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp = p1 - p2;
+            if (tmp.tu == 0)
+                return false;
+            else
+                return true;
+        }
+        public static bool operator >(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp = p1 - p2;
+            if ((tmp.tu>0)&&(tmp.mau>0))
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp = p1 - p2;
+            if (((tmp.tu < 0) && (tmp.mau > 0))|| ((tmp.tu > 0) && (tmp.mau < 0)))
+                return true;
+            else
+                return false;
+        }
+        public static bool operator >=(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp = p1 - p2;
+            if ((tmp.tu >= 0) && (tmp.mau > 0))
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <=(phanso p1, phanso p2)
+        {
+            phanso tmp = new phanso();
+            tmp = p1 - p2;
+            if (((tmp.tu <= 0) && (tmp.mau > 0)) || ((tmp.tu >= 0) && (tmp.mau < 0)))
+                return true;
+            else
+                return false;
+        }
+        public static phanso ToPhanSo(int x)
+        {
+            phanso tmp=new phanso();
+            tmp.tu = x;
+            tmp.mau = 1;
+            return tmp;
+        }
+        public static float ToSingleByPS(phanso p1)
+        {
+            float c = (float)p1.tu / (float)p1.mau;
+            return c;
+        }
+    }
+    public class PhanSo : IComparable
+    {
+        private phanso p = new phanso();
+        public void nhappso()
+        {
+            p.nhap();
+        }
+        public void xuatpso()
+        {
+            p.xuat();
+        }
+        public int CompareTo(Object obj)
+        {
+            PhanSo PhSoToCompare = (PhanSo)obj;
+            if (this.p == PhSoToCompare.p)
+                return 0;
+            if (this.p > PhSoToCompare.p)
+                return 1;
+            else
+                return -1;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            phanso p1 = new phanso();
+            phanso tmp = new phanso();
+            phanso p2 = new phanso();
+            Console.WriteLine("Nhap phan so thu nhat");
+            p1.nhap();
+            Console.WriteLine("Nhap phan so thu hai");
+            p2.nhap();
+            tmp = p1 + p2;
+            Console.WriteLine("Ket qua la ");
+            tmp.xuat();
+            if (p1 > p2)
+                Console.WriteLine("Phan so thu nhat lon hon");
+            if (p1 == p2)
+                Console.WriteLine("Hai phan so bang nhau");
+            if (p1 < p2)
+                Console.WriteLine("Phan so thu nhat nho hon");
+            PhanSo phanso1 = new PhanSo();
+            Console.WriteLine("Nhap phan so thu nhat cua day");
+            phanso1.nhappso();
+            PhanSo phanso2 = new PhanSo();
+            Console.WriteLine("Nhap phan so thu hai cua day");
+            phanso2.nhappso();
+            PhanSo phanso3 = new PhanSo();
+            Console.WriteLine("Nhap phan so thu ba cua day");
+            phanso3.nhappso();
+            PhanSo phanso4 = new PhanSo();
+            Console.WriteLine("Nhap phan so thu tu cua day");
+            phanso4.nhappso();
+            PhanSo phanso5 = new PhanSo();
+            Console.WriteLine("Nhap phan so thu nam cua day");
+            phanso5.nhappso();
+            List<PhanSo> list = new List<PhanSo>();
+            list.Add(phanso1);
+            list.Add(phanso2);
+            list.Add(phanso3);
+            list.Add(phanso4);
+            list.Add(phanso5);
+            list.Sort();
+            Console.WriteLine();
+            Console.WriteLine("Thu tu cac phan so tu nho den lon sau khi duoc sap xep");
+            foreach(PhanSo phanso in list)
+            {
+                phanso.xuatpso();
+            }
+        }
+    }
+}
+
