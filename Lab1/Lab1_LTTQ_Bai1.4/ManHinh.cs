9f352ccbaf7d7df23aca1fc58f5d5fde1f26cb42
piemanscharf95 <22521028@gm.uit.edu.vn>
2023-10-16 14:29:13 +0700

    Add new file 1.3 and 1.4

---
+++ Lab1/Lab1_LTTQ_Bai1.4/ManHinh.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Event
+{
+    public class ManHinh
+    {
+        public ManHinh(NhietKe nhietke)
+        {
+            nhietke.ThayDoiNhietDo += new NhietKe.NhietKeHandler(HienThi);
+        }
+        public void HienThi(object obj, ThongTinNhietDo ttnd)
+        {
+            Console.WriteLine("Nhiet do hien tai la {0}", ttnd.NhietDo);
+        }
+    }
+}
---
+++ Lab1/Lab1_LTTQ_Bai1.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+namespace Event
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            NhietKe nhietke = new NhietKe(28);
+            ManHinh manhinh = new ManHinh(nhietke);
+            for (; ; )
+            {
+                Thread.Sleep(1000);
+                Random random = new Random();
+                int nhietdo = random.Next(10) - 5;
+                nhietke.NhietDo += nhietdo;
+            }
+            Console.ReadLine();
+        }
+    }
+}
